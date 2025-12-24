aa64a1e59bf34c057e0712a7c6f30e8c1315f1c0
Rohit Satpathy <rohitsatpathy2007@gmail.com>
2025-12-24 11:25:11 -0500

    Complete Tasks 2–4: Redux cart, ProductList, CartItem

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'reasct';
 import './ProductList.css'
 import { useDispatch, useSelector } from 'react-redux';
 import CartItem from './CartItem';
